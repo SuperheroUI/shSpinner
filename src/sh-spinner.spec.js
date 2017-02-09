b3370bd01946c6519b06076137c504ac5560d156
kevinlbatchelor <kevinbatchelor@gmail.com>
2017-02-09 10:06:19 -0700

    some tests

--- src/sh-spinner.spec.js
+++ src/sh-spinner.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import TestUtils from 'react-dom/lib/ReactTestUtils';
 import ShSpinner from './sh-spinner';
 
-describe('root', function() {
+describe('root', function () {
     beforeEach(function () {
         jasmine.clock().install();
     });
@@ -11,22 +11,31 @@ describe('root', function() {
         jasmine.clock().uninstall();
     });
 
-    it('renders without problems', function(done) {
-        let root = TestUtils.renderIntoDocument(<ShSpinner shToggleSpinner={true} shLabel={'Loading'} shClass={'monkey'}/>);
+    it('renders without problems', function (done) {
+        let root = TestUtils.renderIntoDocument(<ShSpinner shToggleSpinner={true} shLabel={'Loading'}
+                                                           shClass={'monkey'}/>);
         jasmine.clock().tick(5000);
-
         expect(root).toBeTruthy();
         done();
     });
 
+    it('make sure initial classes are added', function (done) {
+        let root = TestUtils.renderIntoDocument(<ShSpinner shToggleSpinner={true} shLabel={'Loading'}
+                                                           shClass={'monkey'}/>);
+        jasmine.clock().tick(5000);
+        let comp = TestUtils.findRenderedDOMComponentWithClass(root, 'sh-loader');
+        expect(comp.classList).toContain('sh-loader');
+        expect(comp.classList).toContain('monkey');
+        done();
+    });
 
-    it('renders when props change', function(done) {
-        let root = TestUtils.renderIntoDocument(<ShSpinner shToggleSpinner={true} shLabel={'Loading'} shClass={'monkey'}/>);
+    it('spinner opens when prop is received', function (done) {
+        let root = TestUtils.renderIntoDocument(<ShSpinner shToggleSpinner={true} shLabel={'Loading'}
+                                                           shClass={'monkey'}/>);
         root.componentWillReceiveProps({shToggleSpinner: true});
         jasmine.clock().tick(5000);
-
-        expect(root).toBeTruthy();
-        console.log(root)
-        done();
+        let comp = TestUtils.findRenderedDOMComponentWithClass(root, 'sh-loader-svgs');
+        expect(comp.classList).toContain('sh-loader-svgs');
+        done()
     });
 });
